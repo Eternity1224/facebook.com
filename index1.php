fe1010abb45135500c32ebf524307a01e179cdac
Eternity1224 <elyseanato69@gmail.com>
2025-02-13 06:15:17 +0100

    Create index1.php

---
+++ index1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page pour Ordinateurs</title>
+</head>
+<body>
+    <h1>Bienvenue sur la page pour ordinateurs !</h1>
+</body>
+</html>
